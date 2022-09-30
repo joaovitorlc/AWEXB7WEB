aa12352a5f31f8574b2a1ec9cc9a62621480a4d4
joaovitorlc <vitorjoao.jv626@gmail.com>
2022-09-30 07:23:12 -0300

    Aprendendo js e colocando em prática Para o menu mobile ficar interativo

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,7 +3,7 @@ function menuToggle() {
   let menuOpened = document.getElementById("menu-opened");
 
   if(menuOpened.style.display == "block") {
-    menuOpened.style.display == "none";
+    menuOpened.style.display = "none";
   } else{
     menuOpened.style.display = "block";
   }
